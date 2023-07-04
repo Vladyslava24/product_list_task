06c99c318026a0d79a3da38cb865bd91890c9c2b
Vladyslava Lila <vlada.lada.17@gmail.com>
2023-07-04 15:32:11 +0300

    Product list task - remove as List<dynamic> in products api

--- lib/api/product/products_api.dart
+++ lib/api/product/products_api.dart
@@ -7,7 +7,6 @@ class ProductsApi {
   Future<List<dynamic>> fetch() async {
     final response = await http.get(Uri.https('fakestoreapi.com', '/products'));
     final json = convert.jsonDecode(response.body);
-    return json.map((product) => Product.fromJson(product)).toList()
-        as List<dynamic>;
+    return json.map((product) => Product.fromJson(product)).toList();
   }
 }
